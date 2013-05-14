04974d156713c437cac2d29949d21292393fff1a
Apprentice <apprentice@devbootcamp.com>
2013-05-13 23:05:18 -0700

    ajaxed

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,19 +2,17 @@ get '/' do
   erb :index
 end
 
-get '/:word' do
+# get '/:word' do
+#   dictionary = Word.all.map { |w| w.text }
+#   @word = params[:word]
+#   @anagrams = anagrams_for(@word, dictionary)
+#   erb :index
+# end
+
+
+get '/word' do
   dictionary = Word.all.map { |w| w.text }
   @word = params[:word]
   @anagrams = anagrams_for(@word, dictionary)
-  erb :index
 
-end
-
-
-post '/word' do
-  redirect "/#{params[:word]}"
-  # dictionary = Word.all.map { |w| w.text }
-  # @word = params[:word]
-  # @anagrams = anagrams_for(@word, dictionary)
-  # erb :index
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      type: 'get',
+      url: '/word',
+      data: $('form').serialize()
+    }).done(function(data){
+      console.log(data);
+      $('#slider').text(data);
+    });
+  });
 });
